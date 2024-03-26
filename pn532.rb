c7429f4c67cd6058d657e7d9bbe4c7cc8fecb0f5
SkyWallker03 <160876047+SkyWallker03@users.noreply.github.com>
2024-03-26 11:51:53 +0100

    Update pn532.rb

--- pn532.rb
+++ pn532.rb
@@ -1,1 +1,14 @@
+require 'ruby-nfc'
+require 'logger'
 
+class Rfid
+  
+	def read_uid
+		reader = NFC::Reader.all
+		reader[0].poll(Mifare::Classic::Tag) do |tag|
+		 
+			uid = tag.uid_hex.upcase
+			return uid			
+		end
+	end
+end
